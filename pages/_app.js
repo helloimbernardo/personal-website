e612000998c023864154269fe72eb33dfb60d220
Bernardo Oliveira <43864475+helloimbernardo@users.noreply.github.com>
2021-11-07 11:29:43 +0000

    Website version 2.0

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+import '../styles/globals.css'
+import "@fontsource/zen-dots"
+import "@fontsource/jetbrains-mono"
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
